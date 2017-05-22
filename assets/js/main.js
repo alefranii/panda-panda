e36adfee839f19e0b21477fc194939804da63049
alefranii <afmormar@gmail.com>
2017-05-22 00:26:52 -0400

    mejora botones, falta fondo y boton de devolver imagenes

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,16 @@
 	});
 })()
 
+document.getElementById("origin-izq").addEventListener("click",function(){
+	var letraIz = document.getElementById("left");
+	letraIz.classList.toggle("display");
+})
+
+document.getElementById("extincion-derecha").addEventListener("click",function(){
+	var letraDer = document.getElementById("right");
+	letraDer.classList.toggle("display");
+})
+
 
 
 
